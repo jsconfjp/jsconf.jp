d4996ccfbb2c7e927fd971be536c8c1ebc8217af
Leko <leko.noor@gmail.com>
2019-07-12 01:58:24 +0900

    List speakers

--- 2019/src/pages/speakers.tsx
+++ 2019/src/pages/speakers.tsx
@@ -1,15 +1,47 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { useTranslation } from "react-i18next"
 
 import { Layout } from "../components/Layout"
 import { SEO } from "../components/Seo"
 
 export default function SpeakersPage() {
+  const data = useStaticQuery(graphql`
+    query Speakers {
+      allSpeakersYaml {
+        edges {
+          node {
+            featured
+            name
+            github
+            twitter
+            photo
+            biography
+          }
+        }
+      }
+    }
+  `)
   const { t } = useTranslation()
+
   return (
     <Layout>
       <SEO title="Speakers" />
-      TODO: Speakers
+      {data.allSpeakersYaml.edges.map(({ node }) => (
+        <div key={node.name}>
+          <h2>{node.name}</h2>
+          <img src={node.photo} width="100" />
+          <p>{node.biography}</p>
+          <p>
+            <a href={node.github} target="_blank">
+              GitHub
+            </a>
+            <a href={node.twitter} target="_blank">
+              Twitter
+            </a>
+          </p>
+        </div>
+      ))}
     </Layout>
   )
 }
