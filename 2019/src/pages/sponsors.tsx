da1218f95e9d536bb871eba17ab491477a1f5261
Leko <leko.noor@gmail.com>
2019-07-12 01:53:54 +0900

    List sponsors

--- 2019/src/pages/sponsors.tsx
+++ 2019/src/pages/sponsors.tsx
@@ -1,15 +1,38 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { useTranslation } from "react-i18next"
 
 import { Layout } from "../components/Layout"
 import { SEO } from "../components/Seo"
 
 export default function SponsorsPage() {
+  const data = useStaticQuery(graphql`
+    query Sponsors {
+      allSponsorsYaml {
+        edges {
+          node {
+            name
+            url
+            pr
+          }
+        }
+      }
+    }
+  `)
   const { t } = useTranslation()
+
   return (
     <Layout>
       <SEO title="Sponsors" />
-      TODO: Sponsors
+      {data.allSponsorsYaml.edges.map(({ node }) => (
+        <div key={node.name}>
+          <h2>{node.name}</h2>
+          <a href={node.url} target="_blank">
+            {node.url}
+          </a>
+          <p>{node.name}</p>
+        </div>
+      ))}
     </Layout>
   )
 }
