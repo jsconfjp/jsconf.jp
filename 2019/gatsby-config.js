66c777cd762137d49e5e3a214330e26f2bb9d50f
Leko <leko.noor@gmail.com>
2019-07-12 01:49:23 +0900

    List members

--- 2019/gatsby-config.js
+++ 2019/gatsby-config.js
@@ -15,6 +15,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/data`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- 2019/src/pages/about.tsx
+++ 2019/src/pages/about.tsx
@@ -1,15 +1,34 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import { useTranslation } from "react-i18next"
 
 import { Layout } from "../components/Layout"
 import { SEO } from "../components/Seo"
 
 export default function AboutUsPage() {
-  const { t } = useTranslation()
+  const data = useStaticQuery(graphql`
+    query Members {
+      allMembersYaml {
+        edges {
+          node {
+            twitter
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <SEO title="About us" />
-      TODO: About us
+      {data.allMembersYaml.edges.map(({ node }) => (
+        <div key={node}>
+          <img src={`https://twitter.com/${node.twitter}/profile_image`} />
+          <a target="_blank" href={`https://twitter.com/${node.twitter}`}>
+            @{node.twitter}
+          </a>
+        </div>
+      ))}
     </Layout>
   )
 }
