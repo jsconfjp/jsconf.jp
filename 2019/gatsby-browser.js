ba035bd99ba9dcb407bb5806bc558e903f75a86c
Leko <leko.noor@gmail.com>
2019-07-12 00:24:37 +0900

    Configure react-i18net

--- 2019/gatsby-browser.js
+++ 2019/gatsby-browser.js
@@ -1,7 +1,25 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import i18n from "i18next"
+import { initReactI18next } from "react-i18next"
 
-// You can delete this file if you're not using it
+// the translations
+// (tip move them in a JSON file and import them)
+const resources = {
+  en: {
+    translation: {
+      "welcome": "(TODO: Welcome message)",
+    },
+  },
+}
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: "en",
+
+    keySeparator: false, // we do not use keys in form messages.welcome
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  })
--- 2019/src/pages/index.js
+++ 2019/src/pages/index.js
@@ -2,20 +2,17 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import { useTranslation } from "react-i18next";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const { t } = useTranslation()
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{t('welcome')}</h1>
+    </Layout>
+  )
+}
 
 export default IndexPage
