2f5bb996c57e279f99acff984610ac70380cc6b7
Leko <leko.noor@gmail.com>
2019-07-12 10:46:41 +0900

    Add Venue

--- 2019/gatsby-browser.js
+++ 2019/gatsby-browser.js
@@ -4,16 +4,20 @@ import { initReactI18next } from "react-i18next"
 
 // the translations
 // (tip move them in a JSON file and import them)
-const resources = {
-  en: {
-    translation: {
-      "welcome": "(TODO: Welcome message)",
-    },
+const en = {
+  translation: {
+    "welcome": "(TODO: Welcome message)",
+    "venue.name": "3331 Arts Chiyoda",
+    "venue.address": "〒101-0021 6-11-14 Sotokanda Chiyoda-ku Tokyo",
+    "venue.url": "https://www.3331.jp/rentalspace/"
   },
-  ja: {
-    translation: {
-      "welcome": "(TODO: ようこそ)",
-    },
+}
+const ja = {
+  translation: {
+    "welcome": "(TODO: ようこそ)",
+    "venue.name": "アーツ千代田 3331",
+    "venue.address": "〒101-0021 東京都千代田区外神田6丁目11-14",
+    "venue.url": "https://www.3331.jp/rentalspace/"
   },
 }
 
@@ -21,7 +25,7 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
+    resources: { en, ja },
     fallbackLng: 'en',
     keySeparator: false, // we do not use keys in form messages.welcome
 
--- 2019/src/pages/venue.tsx
+++ 2019/src/pages/venue.tsx
@@ -9,7 +9,22 @@ export default function VenuePage() {
   return (
     <Layout>
       <SEO title="Venue" />
-      TODO: Venue
+      <iframe
+        width="600"
+        height="450"
+        style={{ border: 0 }}
+        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ6Xx_rh-MGGAR3sDINOeBYw8&key=AIzaSyAhtEqpuBA3ChFU30d4A1bSODO-anJ54Vo"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+      <p>
+        <h3>{t("venue.name")}</h3>
+        <small>{t("venue.address")}</small>
+        <br />
+        <a target="_blank" href={t("venue.url")}>
+          {t("venue.url")}
+        </a>
+      </p>
     </Layout>
   )
 }
