45e5bd4f821afca98386291ad69bf5fc35572c8d
Leko <leko.noor@gmail.com>
2019-07-17 21:02:02 +0900

    Configure favicon and manifests

--- 2019/gatsby-config.js
+++ 2019/gatsby-config.js
@@ -28,13 +28,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `JSConf JP`,
+        short_name: `JSConf JP`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
