f3d63d175d328fc530bc2505da3c72998a1ed84d
Leko <leko.noor@gmail.com>
2019-07-18 02:29:03 +0900

    Fix typo

--- 2019/src/components/Map.tsx
+++ 2019/src/components/Map.tsx
@@ -13,8 +13,8 @@ export function Map(props: Props) {
       height={height}
       style={{ border: 0 }}
       src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ6Xx_rh-MGGAR3sDINOeBYw8&key=AIzaSyAhtEqpuBA3ChFU30d4A1bSODO-anJ54Vo"
-      frameborder="0"
-      allowfullscreen
+      frameBorder="0"
+      allowFullScreen
     />
   )
 }
