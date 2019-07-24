59b2ec54f7e7931ca3b4569ad83868617a0cad5c
Leko <leko.noor@gmail.com>
2019-07-24 13:23:36 +0900

    apply padding

--- 2019/src/components/Card.tsx
+++ 2019/src/components/Card.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 
 const Box = styled.div`
+  padding: 70px;
   box-shadow: 0px 3px 10px ${({ theme }) => theme.colors.shadow};
   background-color: ${({ theme }) => theme.colors.base};
 `
