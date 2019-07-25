f4a66e02e246173f2bd1444e4081ba8fa2a3186a
Leko <leko.noor@gmail.com>
2019-07-26 02:35:11 +0900

    Set typography for SubTitle

--- 2019/src/components/SubTitle.tsx
+++ 2019/src/components/SubTitle.tsx
@@ -1,11 +1,18 @@
 import React from "react"
+import styled from "styled-components"
 
 export type Props = {
   children: React.ReactNode
 }
 
+const Box = styled.h2`
+  text-transform: uppercase;
+  font-family: ${({ theme }) => theme.fonts.header};
+  font-size: 40px;
+`
+
 export function SubTitle(props: Props) {
   const { children } = props
 
-  return <h2 style={{ textTransform: "uppercase" }}>{children}</h2>
+  return <Box>{children}</Box>
 }
