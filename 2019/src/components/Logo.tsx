a3d08332457e81200c2abb7e05d2234037171155
Leko <leko.noor@gmail.com>
2019-07-26 01:39:16 +0900

    Replace logo with SVG

--- 2019/src/components/Logo.tsx
+++ 2019/src/components/Logo.tsx
@@ -1,6 +1,5 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import logo from "../images/logo.svg"
 
 type Props = {
   size: number
@@ -8,26 +7,8 @@ type Props = {
 
 export function Logo(props: Props) {
   const { size } = props
-  const { file } = useStaticQuery(
-    graphql`
-      query {
-        file(relativePath: { eq: "logo.png" }) {
-          childImageSharp {
-            fixed(width: 125, height: 125) {
-              ...GatsbyImageSharpFixed
-            }
-          }
-        }
-      }
-    `,
-  )
 
-  return (
-    <Img
-      style={{ width: size, height: size }}
-      fixed={file.childImageSharp.fixed}
-    />
-  )
+  return <img style={{ width: size, height: size }} src={logo} />
 }
 
 Logo.defaultProps = {
