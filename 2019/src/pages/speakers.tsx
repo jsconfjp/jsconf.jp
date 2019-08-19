e2ccb0912cd9c41746411df929db13019c0e5205
Leko <leko.noor@gmail.com>
2019-08-19 23:17:22 +0900

    Remove github field from speakers

--- 2019/src/pages/speakers.tsx
+++ 2019/src/pages/speakers.tsx
@@ -17,7 +17,6 @@ export default function SpeakersPage() {
           node {
             featured
             name
-            github
             twitter
             photoURL
             talkTitle
