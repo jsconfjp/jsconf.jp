a732b4233a1f428ccf121fba000cfdde164f5b18
Leko <leko.noor@gmail.com>
2021-07-12 17:51:49 +0800

    chore: copy i18n

---
+++ 2021/src/i18n/en.ts
@@ -0,0 +1,81 @@
+export const en = {
+  translation: {
+    // If you want to override OGP description,
+    // you can add custom description by editing this lines.
+    "about.description": "",
+    "schedule.description": "",
+    "speakers.description": "",
+    "sponsors.description": "",
+    "timetable.description": "",
+
+    backToTop: "TOP",
+    siteName: "JSConf JP",
+    festivalPeriod: "November 30th, December 1st 2019",
+    description:
+      "jsconf.jp is a JavaScript festival in Japan powered by Japan Node.js Association. This is the first event of jsconf in Japan. We would love to become a bridge between Japanese Web Developers and International Web Developers.",
+    speakers: "Speakers",
+    timetable: "Timetable",
+    aboutUs: "About us",
+    organizingTeam: "Organizing team",
+    volunteerTeam: "Volunteer team",
+    joinUs: "Join us",
+    guestSpeakers: "Guest speakers",
+    goToGuests: "CHECK ALL SPEAKERS",
+    schedule: "schedule",
+    day1: "Day 1",
+    day2: "Day 2",
+    roomA: "Room A(Gymnasium)",
+    roomB: "Room B(B105)",
+    roomC: "Room C(Rooftop)",
+    tickets: "Tickets",
+    buyTickets: "Buy Tickets",
+    comingSoon: "Coming soon",
+    "jp-specified-commercial-transactions-act": "特定商取引法に基づく表示",
+    "code-of-conduct": "Code of Conduct",
+    venue: "Venue",
+    moreDetails: "More details",
+    callForSponsors: "Call For Sponsors",
+    becomeASponsor: "Become a sponsor",
+    sponsors: "Sponsors",
+    "sponsor.platinum": "Platinum",
+    "sponsor.gold": "Gold",
+    "sponsor.silver": "Silver",
+    "sponsor.bronze": "Bronze",
+    "venue.name": "3331 Arts Chiyoda",
+    "venue.address": "〒101-0021 6-11-14 Sotokanda Chiyoda-ku Tokyo",
+    "venue.url": "https://www.3331.jp/rentalspace/",
+    "venue.additionalMap":
+      "View map of the vicinity around the venue with hotels and things",
+    "venue.accessBytrain": "ACCESS BY TRAIN",
+    "venue.accessByPlane": "ACCESS BY PLANE",
+    "venue.accessByTokyoMetroGinzaLine":
+      "1 min. from Tokyo Metro Ginza Line Suehirocho station (Exit 4)",
+    "venue.accessByTokyoMetroChiyodaLine":
+      "3 min. from Tokyo Metro Chiyoda Line Yushima station (Exit 6)",
+    "venue.accessByToeiOedoLine":
+      "6 min. from Toei Oedo Line Ueno-Okachimachi station (Exit A1)",
+    "venue.accessByJrOkachimachiStation":
+      "7 min. from JR Okachimachi station (Southern Exit)",
+    "venue.accessByJrAkihabaraStation":
+      "8 min. from JR Akihabara station (Electric City Exit)",
+    "venue.accessByNaritaAirport":
+      "Approx. 70 minutes from Narita Airport to JR Akihabara Station",
+    "venue.accessByHanedaAirport":
+      "Approx. 40 minutes from Haneda Airport to JR Akihabara Station",
+    "venue.accessibilityTitle": "ACCESSIBILITY",
+    "venue.accessibility": `
+・A parking space is provided for the disabled. Please call 03-6803-2441 or email to info(at)3331.jp to make a reservation.
+・Ramped entrance with call bell
+・Elevator (B1F – Rooftop) *Rooftop opens only during
+・Accessible Restroom (1F)
+・Written Communication
+・Assistance Dog Allowed
+・Wheelchair Rental (Please ask at Information Desk)
+・Admission is free for holders of Disability Certificate and their escorts (up to one per certificate holder). If you are living overseas, please bring relevant proof of status
+    `,
+    openMobileMenu: "Open Navigation Menu",
+    closeMobileMenu: "Close Navigation Menu",
+    "jna-address":
+      "〒210-0024 27-7-903 Nisshincho Kawasaki-ku Kawasaki-shi Kanagawa"
+  }
+}
---
+++ 2021/src/i18n/ja.ts
@@ -0,0 +1,60 @@
+import { en } from "./en"
+
+type AvailableKeys = keyof typeof en["translation"]
+
+export const ja: {
+  translation: Partial<{ [K in AvailableKeys]: string }>
+} = {
+  translation: {
+    speakers: "スピーカー",
+    sponsors: "スポンサー",
+    backToTop: "TOPへ戻る",
+    festivalPeriod: "2019年11月30〜12月1日開催",
+    guestSpeakers: "ゲストスピーカー",
+    goToGuests: "スピーカー一覧へ",
+    tickets: "チケット",
+    buyTickets: "チケットを購入する",
+    callForSponsors: "スポンサー募集",
+    becomeASponsor: "スポンサー申込みフォームへ",
+    schedule: "開催スケジュール",
+    roomA: "Room A(体育館)",
+    roomB: "Room B(B105)",
+    roomC: "Room C(屋上)",
+    "jp-specified-commercial-transactions-act": "特定商取引法に基づく表示",
+    "code-of-conduct": "行動規範",
+    venue: "会場アクセス",
+    "venue.name": "アーツ千代田 3331",
+    "venue.address": "〒101-0021 東京都千代田区外神田6丁目11-14",
+    "venue.url": "https://www.3331.jp/rentalspace/",
+    "venue.accessBytrain": "電車でのアクセス",
+    "venue.accessByTokyoMetroGinzaLine":
+      "東京メトロ銀座線 末広町駅 4番出口から徒歩1分",
+    "venue.accessByTokyoMetroChiyodaLine":
+      "東京メトロ千代田線 湯島駅 6番出口から徒歩3分",
+    "venue.accessByToeiOedoLine": "都営大江戸線 上野御徒町駅 A1出口から徒歩6分",
+    "venue.accessByJrOkachimachiStation": "JR 御徒町駅 南口から徒歩7分",
+    "venue.accessByJrAkihabaraStation": "JR 秋葉原駅 電気街口から徒歩8分",
+    "venue.accessByPlane": "飛行機でのアクセス",
+    moreDetails: "詳細",
+    "venue.accessByNaritaAirport": "成田空港からJR 秋葉原駅まで約70分",
+    "venue.accessByHanedaAirport": "羽田空港からJR 秋葉原駅まで約40分",
+    // @ts-ignore It's not defined in en.ts
+    Japanese: "日本語",
+    // @ts-ignore It's not defined in en.ts
+    English: "英語",
+    "venue.accessibility": `
+・会場には障害がある人用の駐車スペースを用意しております。
+・ご利用の際には、事前に 03-6803-2441 、またはinfo(at)3331.jp 宛にご連絡お願いします。
+・呼び出しベル付きの段差スロープ
+・エレベータ (B1F – 屋上) 
+・アクセス可能な洗面所(1F)
+・手記での会話
+・補助犬の同伴可
+・車椅子レンタル (フロントにてお問い合わせください)
+・障害がある人、及びエスコートする人の参加料は無料となります（エスコートは障害がある人一人につき一人まで）。身障手帳などの証明書を持参してください。 海外にお住みの方は、同等の証明書を提示してください。
+`,
+    openMobileMenu: "ナビゲーションを開く",
+    closeMobileMenu: "ナビゲーションを閉じる",
+    "jna-address": "〒210-0024 神奈川県川崎市川崎区日進町27-7-903"
+  }
+}
