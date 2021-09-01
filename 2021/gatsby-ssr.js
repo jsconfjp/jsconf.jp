84a5367de3ca3dc97c24c325979fb6a3e4cdf7c8
Leko <leko.noor@gmail.com>
2021-09-01 19:08:44 +0800

    fix translation during gatsby build

--- 2021/gatsby-ssr.js
+++ 2021/gatsby-ssr.js
@@ -1,7 +1,13 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/ssr-apis/
- */
+import * as React from "react"
+import i18n from "i18next"
+import { I18nextProvider } from "react-i18next"
+import { options } from "./src/i18n/index"
 
-// You can delete this file if you're not using it
+const wrapPageElement = ({ element }) => {
+  i18n.init({
+    ...options
+  })
+  return <I18nextProvider i18n={i18n}>{element}</I18nextProvider>
+}
+
+export { wrapPageElement }
