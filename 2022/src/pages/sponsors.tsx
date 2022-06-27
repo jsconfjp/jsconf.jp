2b8687a8be9a53962908fa35f6ba16434603f133
Leko <leko.noor@gmail.com>
2022-06-27 10:42:06 +0900

    chore: fix tsc errors

--- 2022/src/pages/sponsors.tsx
+++ 2022/src/pages/sponsors.tsx
@@ -1,31 +1,31 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 import { useTranslation } from "react-i18next"
 
 import { Layout } from "../components/Layout"
 import { SEO } from "../components/Seo"
 import { Title } from "../components/Title"
-import { SponsorList } from "../components/SponsorList"
+// import { SponsorList } from "../components/SponsorList"
 import { ResponsiveBox } from "../components/ResponsiveBox"
 import { Breadcrumb } from "../components/Breadcrumb"
 
 export default function SponsorsPage() {
-  const data = useStaticQuery(graphql`
-    query Sponsors {
-      allSponsorsYaml {
-        edges {
-          node {
-            name
-            grade
-            url
-            logoUrl
-            prText
-          }
-        }
-      }
-    }
-  `)
-  const sponsors = data.allSponsorsYaml.edges.map(({ node }: any) => node)
+  // const data = useStaticQuery(graphql`
+  //   query Sponsors {
+  //     allSponsorsYaml {
+  //       edges {
+  //         node {
+  //           name
+  //           grade
+  //           url
+  //           logoUrl
+  //           prText
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  // const sponsors = data.allSponsorsYaml.edges.map(({ node }: any) => node)
   const { t } = useTranslation()
 
   return (
