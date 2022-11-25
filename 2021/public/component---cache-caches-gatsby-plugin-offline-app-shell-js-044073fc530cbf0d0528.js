275045341d4a4d5368b59ece13985868450497bf
yu1k <48705423+yu1k@users.noreply.github.com>
2022-11-25 17:07:33 +0900

    remove old 2021/public directory

--- 2021/public/component---cache-caches-gatsby-plugin-offline-app-shell-js-044073fc530cbf0d0528.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkjsconfjp_2021=self.webpackChunkjsconfjp_2021||[]).push([[306],{1930:function(e,t,n){var u=n(5318);t.__esModule=!0,t.default=void 0;var f=u(n(5354)),r=u(n(7294)),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,f.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),l=a;t.default=l}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-044073fc530cbf0d0528.js.map
