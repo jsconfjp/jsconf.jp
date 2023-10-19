ce97ec28881bd6487b6e090e0ed19378583b3310
yu1k <48705423+yu1k@users.noreply.github.com>
2023-10-19 11:19:04 +0900

    update 2023/11/26 -> 2023/11/19

--- 2023/src/util/misc.ts
+++ 2023/src/util/misc.ts
@@ -1,7 +1,7 @@
 export const times = {
   day1: {
-    startsAt: new Date(2023, 10, 26, 10),
-    endsAt: new Date(2023, 10, 26, 21),
+    startsAt: new Date(2023, 10, 19, 10),
+    endsAt: new Date(2023, 10, 19, 21),
   },
 }
 export type Dates = keyof typeof times
