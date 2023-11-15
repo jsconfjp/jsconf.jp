7903a95135749cc9ea6d21041d20af5469c0f211
Martin Heidegger <martin.heidegger@gmail.com>
2023-11-16 00:40:08 +0900

    feat: useEnOrJa hook

--- 2023/src/util/languages.ts
+++ 2023/src/util/languages.ts
@@ -1,10 +1,20 @@
 import { i18n } from "i18next"
+import { useMemo } from "react"
+import { useTranslation } from "react-i18next"
+
+export const isJapanese = (lang: string) => /ja(-\w)*/.test(lang)
 
 export function enOrJa(i18n: i18n) {
   return (enStr: string, jaStr: string) =>
     isJapanese(i18n.language) && jaStr ? jaStr : enStr || jaStr
 }
 
-export function isJapanese(lang: string) {
-  return /ja(-\w)*/.test(lang)
+const ja = <T>(enStr: T, jaStr: T) => jaStr ?? enStr
+const en = <T>(enStr: T, jaStr: T) => enStr ?? jaStr
+
+export function useEnOrJa() {
+  const {
+    i18n: { language },
+  } = useTranslation()
+  return useMemo(() => (isJapanese(language) ? ja : en), [language])
 }
