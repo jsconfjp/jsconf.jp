a5fd8220f0dbd88a266f2975a0be4a59286b44bb
Martin Heidegger <martin.heidegger@gmail.com>
2024-10-19 00:31:28 +0900

    feat: adding stream mention for room B & C

--- 2024/src/i18n/en.ts
+++ 2024/src/i18n/en.ts
@@ -64,6 +64,7 @@ export const en = {
       "〒210-0024 27-7-903 Nisshincho Kawasaki-ku Kawasaki-shi Kanagawa",
     "talk.open": "Doors Open",
     "talk.opening talk": "Opening Talk",
+    "talk.opening talk stream": "Opening Talk (Stream)",
     "talk.Break": "Break",
     "talk.closing talk": "Closing Talk",
     "talk.party": "Party",
