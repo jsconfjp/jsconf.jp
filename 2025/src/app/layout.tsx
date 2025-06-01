f767f774de0d408fa26c5ec5ca9fd083fd990dde
Leko <leko.noor@gmail.com>
2025-06-01 22:25:43 +0900

    fix: a11y issues

--- 2025/src/app/layout.tsx
+++ 2025/src/app/layout.tsx
@@ -5,7 +5,6 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
-  userScalable: false,
 };
 
 export async function generateMetadata(): Promise<Metadata> {
