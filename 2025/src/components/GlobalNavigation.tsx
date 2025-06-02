b0af669e87453698220a8cff57959e3c1ef740eb
Leko <leko.noor@gmail.com>
2025-06-02 22:09:20 +0900

    feat: add mobile navigation

--- 2025/src/components/GlobalNavigation.tsx
+++ 2025/src/components/GlobalNavigation.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import { useTranslations } from "next-intl";
 import { LanguageSwitcher } from "@/components/LanguageSwitcher";
-import { Link } from "@/i18n/navigation";
+import { Link, usePathname } from "@/i18n/navigation";
+import Bars3Icon from "@heroicons/react/24/outline/Bars3Icon";
+import XMarkIcon from "@heroicons/react/24/outline/XMarkIcon";
 import logoSrc from "@/../public/logo.svg";
+import clsx from "clsx";
+import { useState } from "react";
 
 export function GlobalNavigation() {
+  const [isOpen, setIsOpen] = useState(false);
   const t = useTranslations("navigation");
+  const pathname = usePathname();
   const navigations = [
     {
       ready: false,
@@ -13,7 +21,7 @@ export function GlobalNavigation() {
       href: "/speakers",
     },
     {
-      ready: false,
+      ready: true,
       label: t("venue"),
       href: "/venue",
     },
@@ -30,10 +38,15 @@ export function GlobalNavigation() {
     {
       ready: false,
       label: t("tickets"),
-      href: "/tickets",
+      href: "https://example.com",
       primary: true,
     },
-  ];
+  ].map((item) => ({
+    ...item,
+    active: pathname === item.href,
+  }));
+  const spacerItems = navigations.filter(({ ready }) => !ready);
+  const readyItems = navigations.filter(({ ready }) => ready);
 
   return (
     <header className="shadow-sm bg-white px-2 lg:px-0">
@@ -49,30 +62,67 @@ export function GlobalNavigation() {
           />
         </Link>
         <LanguageSwitcher />
-        <nav className="flex-1 flex items-center gap-4">
-          {navigations.map(({ label, href, primary, ready }) =>
-            ready ? (
-              <Link
-                key={href}
-                href={href}
-                className={
-                  "py-5 h-full flex-1 flex items-center justify-center text-lg font-bold underline underline-offset-8 decoration-4 decoration-transparent " +
-                  (primary
-                    ? "bg-primary text-white hover:decoration-white"
-                    : "hover:decoration-dimmed")
-                }
-              >
-                {label}
-              </Link>
+        <nav className="flex-1 items-center justify-end gap-4 flex lg:hidden">
+          <button
+            type="button"
+            className="text-2xl p-2 cursor-pointer"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            {isOpen ? (
+              <XMarkIcon className="w-6 h-6" />
             ) : (
-              // readyでない要素があっても幅の計算は同一になるように空の要素を入れておく
-              <div key={href} className="py-5 h-full flex-1">
-                &nbsp;
-              </div>
-            )
+              <Bars3Icon className="w-6 h-6" />
+            )}
+          </button>
+        </nav>
+        <nav className="flex-1 items-center gap-4 hidden lg:flex">
+          {[...spacerItems, ...readyItems].map(
+            ({ label, href, primary, ready, active }) =>
+              ready ? (
+                <Link
+                  key={href}
+                  href={href}
+                  className={clsx(
+                    "py-5 h-full flex-1 flex items-center justify-center text-lg font-bold underline underline-offset-8 decoration-4",
+                    primary && "bg-primary text-white hover:decoration-white",
+                    active
+                      ? "decoration-primary"
+                      : "decoration-transparent hover:decoration-dimmed"
+                  )}
+                >
+                  {label}
+                </Link>
+              ) : (
+                // readyでない要素があっても幅の計算は同一になるように空の要素を入れておく
+                <div key={href} className="py-5 h-full flex-1">
+                  &nbsp;
+                </div>
+              )
           )}
         </nav>
       </div>
+      <nav
+        className={clsx(
+          "flex-1 items-center gap-4 border-t-1 border-dimmed divide-y-1 divide-dimmed/50",
+          isOpen ? "visible" : "hidden"
+        )}
+      >
+        {readyItems.map(({ label, href, primary, active }) => (
+          <Link
+            key={href}
+            href={href}
+            className={clsx(
+              "py-4 flex-1 flex items-center justify-center text-md font-bold underline underline-offset-6 decoration-4",
+              primary && "bg-primary text-white hover:decoration-white",
+              active
+                ? "decoration-primary"
+                : "decoration-transparent hover:decoration-dimmed"
+            )}
+          >
+            {label}
+          </Link>
+        ))}
+      </nav>
     </header>
   );
 }
