864add2878428c1a043f3d845af0ac9ea347a318
Leko <leko.noor@gmail.com>
2025-06-04 23:36:58 +0900

    refactor

--- 2025/src/components/Chip.tsx
+++ 2025/src/components/Chip.tsx
@@ -12,24 +12,15 @@ export function Chip({
   track,
   size = "default",
 }: React.PropsWithChildren<Props>) {
-  const backgroundColor =
-    track && track !== "all"
-      ? `var(--color-track-${track.toLowerCase()})`
-      : "var(--color-secondary)";
-  const getTextColor = (track?: Track) => {
-    if (!track || track === "all") return "white";
-    return track === "C" ? "black" : "white";
-  };
-
-  const color = getTextColor(track);
-
   return (
     <span
       className={clsx(
         "flex rounded-md font-bold",
-        size === "default" ? "py-1 px-4 text-sm" : "py-0.5 px-2 text-xs"
+        size === "default" ? "py-1 px-4 text-sm" : "py-0.5 px-2 text-xs",
+        track && track !== "all"
+          ? `bg-track-${track.toLowerCase()} text-track-${track.toLowerCase()}-surface`
+          : "bg-secondary text-white"
       )}
-      style={{ color, backgroundColor }}
     >
       {children}
     </span>
