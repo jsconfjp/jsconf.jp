b665202e53728a20ab0c6dc5b577e60fecd3508c
Leko <leko.noor@gmail.com>
2025-06-05 23:58:43 +0900

    fixup

--- 2025/src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
