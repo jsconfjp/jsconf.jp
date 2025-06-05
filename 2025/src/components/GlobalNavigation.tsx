0d2454a201fa05d57a900697b4bbe0eda67236e0
Leko <leko.noor@gmail.com>
2025-06-05 22:30:40 +0900

    fixup: revert unintended changes

--- 2025/src/components/GlobalNavigation.tsx
+++ 2025/src/components/GlobalNavigation.tsx
@@ -8,8 +8,10 @@ import Bars3Icon from "@heroicons/react/24/outline/Bars3Icon";
 import XMarkIcon from "@heroicons/react/24/outline/XMarkIcon";
 import logoSrc from "@/../public/logo.svg";
 import clsx from "clsx";
+import { useState } from "react";
 
 export function GlobalNavigation() {
+  const [isOpen, setIsOpen] = useState(false);
   const t = useTranslations("navigation");
   const pathname = usePathname();
   const navigations = [
@@ -61,32 +63,18 @@ export function GlobalNavigation() {
         </Link>
         <LanguageSwitcher />
         <nav className="flex-1 items-center justify-end gap-4 flex lg:hidden">
-          <details className="relative [&[open]_:first-child]:hidden [&[open]_:last-child]:block">
-            <summary
-              className="list-none text-2xl p-2 cursor-pointer flex"
-              aria-label="Show/Hide navigations"
-            >
+          <button
+            type="button"
+            className="text-2xl p-2 cursor-pointer"
+            aria-label="Show/Hide navigations"
+            onClick={() => setIsOpen((prev) => !prev)}
+          >
+            {isOpen ? (
+              <XMarkIcon className="w-6 h-6" />
+            ) : (
               <Bars3Icon className="w-6 h-6" />
-              <XMarkIcon className="w-6 h-6 hidden" />
-            </summary>
-            <div className="absolute top-full right-0 left-0 z-10 bg-white border-t border-dimmed divide-y divide-dimmed/50 shadow-lg">
-              {readyItems.map(({ label, href, primary, active }) => (
-                <Link
-                  key={href}
-                  href={href}
-                  className={clsx(
-                    "py-4 flex-1 flex items-center justify-center text-md font-bold underline underline-offset-6 decoration-4",
-                    primary && "bg-primary text-white hover:decoration-white",
-                    active
-                      ? "decoration-primary"
-                      : "decoration-transparent hover:decoration-dimmed"
-                  )}
-                >
-                  {label}
-                </Link>
-              ))}
-            </div>
-          </details>
+            )}
+          </button>
         </nav>
         <nav className="flex-1 items-center gap-4 hidden lg:flex">
           {[...spacerItems, ...readyItems].map(
@@ -114,6 +102,28 @@ export function GlobalNavigation() {
           )}
         </nav>
       </div>
+      <nav
+        className={clsx(
+          "flex-1 items-center gap-4 border-t-1 border-dimmed divide-y-1 divide-dimmed/50",
+          isOpen ? "visible" : "hidden"
+        )}
+      >
+        {readyItems.map(({ label, href, primary, active }) => (
+          <Link
+            key={href}
+            href={href}
+            className={clsx(
+              "py-4 flex-1 flex items-center justify-center text-md font-bold underline underline-offset-6 decoration-4",
+              primary && "bg-primary text-white hover:decoration-white",
+              active
+                ? "decoration-primary"
+                : "decoration-transparent hover:decoration-dimmed"
+            )}
+          >
+            {label}
+          </Link>
+        ))}
+      </nav>
     </header>
   );
 }
