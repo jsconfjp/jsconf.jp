ccca684605ff84c9e1eb4818f349e25836b6db1d
Leko <leko.noor@gmail.com>
2025-09-13 12:19:35 +0900

    ci: add job to cpature screenshots

---
+++ 2025/tests/screenshot.spec.ts
@@ -0,0 +1,35 @@
+import { join } from "node:path";
+import { test } from "@playwright/test";
+import { LOCALES } from "@/i18n/constants";
+import nextConfig from "../next.config";
+
+const DIR_SCREENSHOTS = join(__dirname, "..", "screenshots");
+const prefix = nextConfig.basePath;
+
+const pages = [
+  { label: "Top", url: "/" },
+  { label: "Sponsors", url: "/sponsors" },
+  { label: "Venue", url: "/venue" },
+  { label: "CoC", url: "/doc/code-of-conduct" },
+].flatMap((page) =>
+  LOCALES.map((locale) => ({
+    label: `${page.label} (${locale})`,
+    url: `${prefix}/${locale}/${page.url}`,
+    slug: [locale, ...page.url.split("/")].filter(Boolean).join("-"),
+  }))
+);
+
+pages.forEach(({ label, slug, url }) => {
+  test(label, async ({ page }, testInfo) => {
+    await page.goto(url);
+
+    // Load all media marked as lazy
+    await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+    await page.waitForLoadState("networkidle");
+
+    await page.screenshot({
+      path: join(DIR_SCREENSHOTS, testInfo.project.name, `${slug}.png`),
+      fullPage: true,
+    });
+  });
+});
